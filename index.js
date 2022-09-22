4c8265a6f1d4cf0203c059f9eb0f27ce5ad41326
saketh rudraraju <svrudy1@gmail.com>
2022-09-22 14:56:34 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Configure environment variables
+dotenv.config();
+
+// Configure port
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Set middleware
+app.use(express.json());
+app.use(require("cors")());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
